0a8b615a79c7384d4a092ffe8be70730da1ab1ff
ericrothmuller <eric@skymarketingco.com>
2022-06-22 23:09:14 -0700

    Added uppercaseLetters array with a full list of all uppercase letters.

--- script.js
+++ script.js
@@ -17,6 +17,9 @@ generateBtn.addEventListener("click", writePassword);
 
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
 function windowPrompt() {
     let passwordLength = prompt("How long do you want your password to me?", "Enter a number between 8 and 128");
         if (passwordLength >= 8 && passwordLength <= 128) {
