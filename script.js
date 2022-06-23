04a4e17238ff0dbc04016dd8843ca5601a1fa043
ericrothmuller <eric@skymarketingco.com>
2022-06-22 23:07:22 -0700

    Added lowercaseLetters array with a full list of lowercase letters in the array

--- script.js
+++ script.js
@@ -15,10 +15,17 @@ generateBtn.addEventListener("click", writePassword);
 
 // Adds Window Prompt
 
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 function windowPrompt() {
     let passwordLength = prompt("How long do you want your password to me?", "Enter a number between 8 and 128");
         if (passwordLength >= 8 && passwordLength <= 128) {
             confirm("Include lowercase letters?")
+
+
+
+
+
     }
     else {
         alert("You must enter a number between 8 and 128.");
