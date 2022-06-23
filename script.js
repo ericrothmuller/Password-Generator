777bfce4e3a3a29f69970d74c5455ba71cf4a3f9
ericrothmuller <eric@skymarketingco.com>
2022-06-22 22:55:52 -0700

    Created window prompt function that properly asks for a password length between 8 and 128 characters and gives an alert if those conditions aren't met repeating what the conditions need to be to proceed.

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Adds Window Prompt
+
+function windowPrompt() {
+    let passwordLength = prompt("How long do you want your password to me?", "Enter a number between 8 and 128");
+        if (passwordLength >= 8 && passwordLength <= 128) {
+            confirm("Include lowercase letters?")
+    }
+    else {
+        alert("You must enter a number between 8 and 128.");
+    }
+}
+
+
+
+
+
+
+
+
+
+
