9a4fcd9f3096e40ec5dabf914e99d74dcaf93e6d
ericrothmuller <eric@skymarketingco.com>
2022-06-23 09:31:20 -0700

    Added specialCharacters string

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var numbers = "0123456789";
 
+var specialCharacters = "!@#$%^&*()";
+
 function windowPrompt() {
     let passwordLength = prompt("How long do you want your password to me?", "Enter a number between 8 and 128");
         if (passwordLength >= 8 && passwordLength <= 128) {
