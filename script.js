f3390f1f4e4fd28b2fab138b7c3b2c905bbf7a8c
ericrothmuller <eric@skymarketingco.com>
2022-06-23 23:59:29 -0700

    Added detailed comments in the Javascript file.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ generateBtn.addEventListener("click", writePassword);
 
 function windowPrompt() {
 
+    // Creates string lists that contain options to select from.
     var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 
     var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -24,61 +25,58 @@ function windowPrompt() {
     var numbers = "0123456789";
     
     var specialCharacters = "!#$%&()*+,-./:;<=>?@[^]_`{|}~";
-    
+
+    // Sets the variable that will contain your selection options to blank
     selectedCharacterOptions = "";
-    
+    // Sets the variable that will eventually contain your password to blank
     yourPassword = "";
 
+    // This is your window prompt for character
     let passwordLength = prompt("How long do you want your password to me?", "Enter a number between 8 and 128");
         if (passwordLength >= 8 && passwordLength <= 128) {
+            // This asks if user wants to include lowercase letters and if so, adds lowercaseLetters to selectedCharacter list
             var includeLowercase = confirm("Include lowercase letters?")
             if (includeLowercase) {
                 selectedCharacterOptions += lowercaseLetters;
             }
 
+            // This asks if user wants to include uppercase letters and if so, adds uppercaseLetters to selectedCharacter list
             var includeUppercase = confirm("Include uppercase letters?")
             if (includeUppercase) {
                 selectedCharacterOptions += uppercaseLetters;
             }
 
+            // This asks if user wants to include numeric characters (numbers) and if so, adds numbers to selectedCharacter list
             var includeNumeric = confirm("Include numeric characters?")
             if (includeNumeric) {
                 selectedCharacterOptions += numbers;
             }
 
+            // This asks if user wants to include special characters and if so, adds specialCharacters to selectedCharacter list
             var includeSpecial = confirm("Include special characters?")
             if (includeSpecial) {
                 selectedCharacterOptions += specialCharacters;
             }
             
+            // This makes sure that at least one of the 4 character options asked above is chosen and if this condition isn't met, it'll alert the user and end password generation
             if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
                 return alert("You must choose to include at least one uppercase, lowercase, numeric, or special character")
             }
 
+            // This runs a loop that produces yourPassword at the user's chosen passwordLength and randomly chooses them from selectedCharacterOptions
             for (let i = 0; i < passwordLength; i++) {
                 yourPassword += selectedCharacterOptions.charAt(
                     Math.floor(Math.random() * selectedCharacterOptions.length)
                 )
                 }
 
-
-                document.getElementById("password").innerHTML = yourPassword;
-
-
-            
-
+            // This inputs yourPassword into the "Your Secure Password" text area so users can see and use it.
+            document.getElementById("password").innerHTML = yourPassword;
+    
     }
+
+    // This alerts the user if they don't initially input a number between 8 and 128.
     else {
         alert("You must enter a number between 8 and 128.");
     }
 }
-
-
-
-
-
-
-
-
-
-
